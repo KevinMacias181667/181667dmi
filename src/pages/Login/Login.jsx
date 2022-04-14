9d473901a030b27c56756186d50f662f7f19ac3f
KevinMacias181667 <181667@utags.edu.mx>
2022-04-13 21:58:04 -0500

    importación libreria expo modules core importación de la libreria i18n

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import {StatusBar} from "expo-status-bar";
 import { Text, View } from 'react-native';
-
 import {styles} from "./Login.styles";
 import ButtonComponent from "../../components/Button";
+import i18n from "../../../localizations/i18n"
 
 export default function Login({onPress}){
 return (
     <View style={styles.container}>
       <StatusBar/>
-      <Text>{i18n.t("Login")}</Text>
+      <Text>Login
+      </Text>
       <ButtonComponent title="Login" onPress={onPress} />
     </View>
   );
